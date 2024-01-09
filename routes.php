463dea00339d0dc832e2d73cf5ee57b405ba4659
almon <almonsoursalida@gmail.com>
2024-01-09 23:40:33 +0800

    aaaa

--- routes.php
+++ routes.php
@@ -1,5 +1,5 @@
 <?php
-// Define your custom routes//
+// Define your custom routes////
 $router->addRoute('GET', '/', 'pageController', 'consumer');
 $router->addRoute('GET', 'consumer', 'pageController', 'consumer');
 $router->addRoute('GET', 'add-consumer', 'pageController', 'addConsumer');
